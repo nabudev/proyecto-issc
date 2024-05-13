8797a86b1b362183a9bae8d9a3528af70648d70a
nahueldev <nabudev01@gmail.com>
2024-05-13 17:08:15 -0300

    carga de primeros datos alumnos, carrera y pagos

--- alumnos/views.py
+++ alumnos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from .models import Pagos
 # Create your views here.
+
+def pagos(request):
+    pagos=list(Pagos.objects.values())
+    return JsonResponse(pagos, safe=False)
