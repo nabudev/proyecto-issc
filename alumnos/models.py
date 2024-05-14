d34ef76322ee04324eb1dc21d3bb44a7f4f1ccb3
nahueldev <nabudev01@gmail.com>
2024-05-13 22:36:48 -0300

    creando funciones para darle formato de texto visualizable al panel de admin

--- alumnos/models.py
+++ alumnos/models.py
@@ -9,12 +9,20 @@ class Carreras(models.Model):
     horario= models.CharField(max_length=20)
     precio=models.IntegerField()
     
+    def __str__(self):
+        return self.nombre
+
+    
 class Alumnos(models.Model):
     dni= models.IntegerField(primary_key=True)
     nombre= models.CharField(max_length=25)
     apellido= models.CharField(max_length=25)
-    edad= models.CharField(max_length=2)
+    direccion= models.CharField(max_length=30)
+    contacto=models.CharField(max_length=40)
     id_carrera= models.ForeignKey(Carreras, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.nombre} {self.apellido} {self.dni}'
     
 class Materias(models.Model):
     id_materia=models.AutoField(primary_key=True)
@@ -23,14 +31,23 @@ class Materias(models.Model):
     horario=models.CharField(max_length=20)
     id_carrera= models.ForeignKey(Carreras, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.nombre
+    
 class Matricula(models.Model):
     id_matricula= models.AutoField(primary_key=True)
     precio_matricula= models.CharField(max_length=11)
     precio_seguro=models.CharField(max_length=10)
     dni= models.ForeignKey(Alumnos,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.dni.dni)
 
 class Metodo_pago(models.Model):
     tipo= models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.tipo
     
 class Pagos(models.Model):
     id_pagos=models.AutoField(primary_key=True)
@@ -40,6 +57,10 @@ class Pagos(models.Model):
     fecha= models.DateField()
     observacion= models.CharField(max_length=50)
     metodo_de_pago=models.ForeignKey(Metodo_pago, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.dni.dni)
+
     
 
     
