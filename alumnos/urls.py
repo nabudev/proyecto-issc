cbd6adcb6a35809d7663813eef32d6e1bdc78563
nahueldev <nabudev01@gmail.com>
2024-05-13 21:50:42 -0300

    creacion de panel administrador

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pagos)
+    path('', views.index),
+    path('pagos/', views.pagos),
+    path('alumnos/', views.alumnos)
 ]
--- alumnos/views.py
+++ alumnos/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Pagos
+from .models import Pagos, Alumnos
 # Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Bienvenidos a la gestion alumnos</h1>')
 
 def pagos(request):
     pagos=list(Pagos.objects.values())
     return JsonResponse(pagos, safe=False)
+
+def alumnos(request):
+    alumnos= list(Alumnos.objects.values())
+    return JsonResponse(alumnos, safe=False)
