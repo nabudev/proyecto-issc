28bab0052f28d45651f0c21db6a6737493805c95
nahueldev <nabudev01@gmail.com>
2024-05-16 20:55:41 -0300

    creando urls names y creando contenido estatico: imagen de logo issc

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('pagos/<int:dni>/', views.pagos),
-    path('alumnos/', views.alumnos)
+    path('', views.index, name='index'),
+    path('pagos/<int:dni>/', views.pagos, name='pagos'),
+    path('alumnos/', views.alumnos, name='alumnos')
 ]
